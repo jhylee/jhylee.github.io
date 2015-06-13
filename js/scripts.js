c1f8c6fcf5c3237dcbb6df47421c2cebbe35b778
jhylee <josephlee@Josephs-MacBook-Air.local>
2015-06-13 16:40:55 -0600

    Visual Updates

--- js/scripts.js
+++ js/scripts.js
@@ -76,7 +76,30 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
 	this.classList.toggle("active");
 });
 
-/***************** Pop Up Video ******************/
+/***************** Pop Up Video ; FANCYBOX ******************/
+/****
+	$(".youtube-media").on("click", function(e) {
+		var jWindow = $(window).width();
+		if (jWindow <= 768) {
+			return;
+		}
+		$.fancybox({
+			href: this.href,
+			padding: 4,
+			type: "iframe",
+			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+		});
+		return false;
+	});
+
+});
+
+$(document).ready(function() {
+	$("a.single_image").fancybox({
+		padding: 4,
+	});
+});
+****/
 
 /***************** Overlays ******************/
 
